4649cb0d9b3521620cb1af51f0c56d3f83800986
Aditya <aditya.enthu@gmail.com>
2016-07-03 11:08:16 +0530

    removing debug logs

--- XYZReader/src/main/java/com/example/xyzreader/ui/FeedDetailActivity.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/FeedDetailActivity.java
@@ -14,7 +14,6 @@ import android.support.v4.app.ShareCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 
